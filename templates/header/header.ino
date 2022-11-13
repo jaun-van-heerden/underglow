7b8e618e51acab93b7143fbeddc4ba14c9a00b65
Kn1feK1ng <118087114+Kn1feK1ng@users.noreply.github.com>
2022-11-13 13:05:03 +1000

    templates
    Added templates for creating new libraries

---
+++ templates/header/header.ino
@@ -0,0 +1,20 @@
+/*
+  Header file template for implementing new libraries
+  Filename must be same as source name with '.h' extension
+
+  https://docs.arduino.cc/learn/contributions/arduino-creating-library-guide
+*/
+#ifndef [Insert Name]_h
+#define [Insert Name]_h
+
+// #include "[Insert Optional Library].h"
+
+class [Insert ClassName]
+{
+  public:
+    // Declare public objects/functions
+  private:
+    // Declare private objects/functions
+};
+
+#endif
---
+++ templates/source/source.ino
@@ -0,0 +1,15 @@
+/*
+  Source file template for implementing new libraries
+  Filename must be same as header fileName
+  
+  https://docs.arduino.cc/learn/contributions/arduino-creating-library-guide
+*/
+
+#include "[Insert Name Library].h"
+// #include "[Insert optional Library].h"
+
+
+[ReturnType][ClassName]::[FunctionName]([Params]); // Declare function/s here
+{
+  
+}
